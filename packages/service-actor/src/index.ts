62650bf9885107ebadcccbdecd8caca8bd6baeb6
Omar Ezzat <omar.ezzat@email.com>
2026-06-10 10:54:58 +0200

    move mime types

--- packages/service-actor/src/index.ts
+++ packages/service-actor/src/index.ts
@@ -1,5 +1,3 @@
 export { ActorService } from './ActorService';
-export type { TActorSandboxHandle, TActorSandboxRunner, TActorSandboxStartArgs, TActorServiceConfig, TActorServiceRuntimeConfig, TActorServiceSendMessageArgs, TActorServiceSendMessageResult, TActorServiceStatus, TActorServiceWidgetSource, TActorServiceWorkerEnv } from './ActorService';
-export { ActorSupervisor } from './ActorSupervisor';
-export type { TActorSupervisorConfig, TActorSupervisorDrainResult, TActorSupervisorRunOnceResult } from './ActorSupervisor';
 export type * from './core/types';
+export * from './core/vibecanvasjson.zod'
